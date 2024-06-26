3095c1061b2a9a01bb093d430340a2171428b24f
Pedro Terraf <106350031+pedroterraf@users.noreply.github.com>
2024-06-26 20:55:34 -0300

    Add/Button&Skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import * as faceapi from 'face-api.js'
+import { Button } from "@mui/material";
+import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
 import './App.css'
 
 function App() {
   const videoRef = useRef()
   const canvasRef = useRef()
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const streamRef = useRef()
 
   // Ejecuto apenas el cliente entra
   useEffect(() => {
-    startVideo()
     videoRef && loadModels()
   }, [])
 
   // Abro la cámara
   const startVideo = () => {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then((currentStream) => {
-        videoRef.current.srcObject = currentStream
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+    if (isVideoPlaying) {
+      // Detener el video
+      const tracks = streamRef.current.getTracks()
+      tracks.forEach(track => track.stop())
+      videoRef.current.srcObject = null
+      canvasRef.current.getContext('2d').clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
+      setIsVideoPlaying(false)
+    } else {
+      // Iniciar el video
+      setIsLoading(true)
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((currentStream) => {
+          streamRef.current = currentStream
+          videoRef.current.srcObject = currentStream
+          videoRef.current.onloadeddata = () => {
+            setIsLoading(false)
+            setIsVideoPlaying(true)
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+          setIsLoading(false)
+        })
+    }
   }
 
   // Cargo los modelos de la API
@@ -77,37 +99,20 @@ function App() {
   return (
     <div className="app">
       <h1 className='app_header_text'>Artificial Intelligence</h1>
-      <video ref={videoRef} autoPlay className='video'></video>
+      {isLoading &&
+        <Skeleton
+          sx={{bgcolor: 'grey.900'}}
+          variant="rectangular"
+          className='video'
+        />
+      }
+      <video ref={videoRef} autoPlay className='video' style={{ display: isLoading ? 'none' : 'block' }}></video>
       <canvas ref={canvasRef} className="canvas" />
+      <Button variant='contained' onClick={startVideo} className='button'>
+        {isVideoPlaying ? 'Stop' : 'Scan me'}
+      </Button>
     </div>
   )
 }
 
-export default App
-
-/* 
-  const faceMyDetect = ()=>{
-    setInterval(async()=>{
-      const detections = await faceapi.detectAllFaces(videoRef.current,
-        new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-
-      //Detecta y muestra el estado
-      canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(videoRef.current)
-      faceapi.matchDimensions(canvasRef.current,{
-        width:940,
-        height:650
-      })
-
-      const resized = faceapi.resizeResults(detections,{
-         width:940,
-        height:650
-      })
-
-      faceapi.draw.drawDetections(canvasRef.current,resized)
-      faceapi.draw.drawFaceLandmarks(canvasRef.current,resized)
-      faceapi.draw.drawFaceExpressions(canvasRef.current,resized)
-      
-    },100)
-  }
-
-*/
+export default App;
